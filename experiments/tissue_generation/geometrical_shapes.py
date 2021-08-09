3f22d10f6a6716ebd3b8753f23b521b09c52b4da
Janek Grohl <janek.grohl@cruk.cam.ac.uk>
2021-08-09 12:59:14 +0100

    add first version scripts for the creation of figures that showcase the versatility of the SIMPA structure library

---
+++ experiments/tissue_generation/geometrical_shapes.py
@@ -0,0 +1,64 @@
+from simpa.utils.libraries.structure_library import SphericalStructure, define_spherical_structure_settings
+from simpa.utils.libraries.structure_library import ParallelepipedStructure, define_parallelepiped_structure_settings
+from simpa.utils.libraries.structure_library import RectangularCuboidStructure, define_rectangular_cuboid_structure_settings
+from simpa.utils.libraries.structure_library import CircularTubularStructure, define_circular_tubular_structure_settings
+from simpa.utils import TISSUE_LIBRARY
+from simpa.utils.deformation_manager import create_deformation_settings
+from simpa.utils import Settings, Tags
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(28374628)
+
+global_settings = Settings()
+global_settings[Tags.SPACING_MM] = 1.0
+global_settings[Tags.DIM_VOLUME_X_MM] = 50
+global_settings[Tags.DIM_VOLUME_Y_MM] = 50
+global_settings[Tags.DIM_VOLUME_Z_MM] = 50
+global_settings.set_volume_creation_settings({
+    Tags.SIMULATE_DEFORMED_LAYERS: True,
+    Tags.DEFORMED_LAYERS_SETTINGS: create_deformation_settings(
+                bounds_mm=[[0, global_settings[Tags.DIM_VOLUME_X_MM]],
+                           [0, global_settings[Tags.DIM_VOLUME_Y_MM]]],
+                maximum_z_elevation_mm=10,
+                filter_sigma=0,
+                cosine_scaling_factor=1)
+})
+
+sphere_settings = define_spherical_structure_settings(start_mm=[25, 20, 33], radius_mm=10,
+                                                      molecular_composition=TISSUE_LIBRARY.epidermis())
+sphere = SphericalStructure(global_settings, sphere_settings).geometrical_volume
+
+tube_settings = define_circular_tubular_structure_settings(tube_start_mm=[10, 0, 30], tube_end_mm=[15, 50, 10],
+                                                           radius_mm=5,
+                                                            molecular_composition=TISSUE_LIBRARY.epidermis())
+tube = CircularTubularStructure(global_settings, tube_settings).geometrical_volume
+
+parallel_settings = define_parallelepiped_structure_settings(start_mm=[15, 25, 25],
+                                                             edge_a_mm=[5, 10, 5],
+                                                             edge_b_mm=[12, 18, 5],
+                                                             edge_c_mm=[25, 0, 15],
+                                                             molecular_composition=TISSUE_LIBRARY.epidermis())
+parallelepiped = ParallelepipedStructure(global_settings, parallel_settings).geometrical_volume
+
+cube_settings = define_rectangular_cuboid_structure_settings(start_mm=[50, 0, 50],
+                                                             extent_mm=[-10, 10, -10],
+                                                             molecular_composition=TISSUE_LIBRARY.epidermis())
+cube = RectangularCuboidStructure(global_settings, cube_settings).geometrical_volume
+
+print("Plotting...")
+
+fig = plt.figure(figsize=(7, 7))
+ax = fig.add_subplot(111, projection='3d')
+ax.voxels(sphere, shade=True, facecolors="blue", alpha=0.55)
+ax.voxels(tube, shade=True, facecolors="green", alpha=0.55)
+ax.voxels(parallelepiped, shade=True, facecolors="yellow", alpha=0.55)
+ax.voxels(cube, shade=True, facecolors="red", alpha=0.55)
+ax.set_aspect('auto')
+ax.set_zlim(50, 0)
+ax.set_zlabel("Depth [mm]")
+ax.set_xlabel("x width [mm]")
+ax.set_ylabel("y width [mm]")
+ax.view_init(elev=10., azim=-45)
+plt.savefig("geometrical_shapes.png", dpi=300)
+plt.show()
---
+++ experiments/tissue_generation/vessel_tree.py
@@ -0,0 +1,55 @@
+from simpa.utils.libraries.structure_library import VesselStructure, define_vessel_structure_settings
+from simpa.utils.libraries.structure_library import HorizontalLayerStructure, define_horizontal_layer_structure_settings
+from simpa.utils import TISSUE_LIBRARY
+from simpa.utils.deformation_manager import create_deformation_settings
+from simpa.utils import Settings, Tags
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(8257387)
+
+global_settings = Settings()
+global_settings[Tags.SPACING_MM] = 1.0
+global_settings[Tags.DIM_VOLUME_X_MM] = 50
+global_settings[Tags.DIM_VOLUME_Y_MM] = 50
+global_settings[Tags.DIM_VOLUME_Z_MM] = 50
+global_settings.set_volume_creation_settings({
+    Tags.SIMULATE_DEFORMED_LAYERS: True,
+    Tags.DEFORMED_LAYERS_SETTINGS: create_deformation_settings(
+                bounds_mm=[[0, global_settings[Tags.DIM_VOLUME_X_MM]],
+                           [0, global_settings[Tags.DIM_VOLUME_Y_MM]]],
+                maximum_z_elevation_mm=10,
+                filter_sigma=0,
+                cosine_scaling_factor=1)
+})
+
+vessel_settings = define_vessel_structure_settings([25, 0, 30], [0, 1, 0],
+                                                   molecular_composition=TISSUE_LIBRARY.blood(0.5),
+                                                   radius_mm=4, bifurcation_length_mm=10, radius_variation_factor=0,
+                                                   curvature_factor=0.1)
+
+vessel = VesselStructure(global_settings, vessel_settings)
+
+vessel_tree = vessel.geometrical_volume
+
+epidermis_settings = define_horizontal_layer_structure_settings(z_start_mm=2, thickness_mm=1,
+                                                                adhere_to_deformation=True,
+                                                                molecular_composition=TISSUE_LIBRARY.epidermis())
+
+epidermis = HorizontalLayerStructure(global_settings, epidermis_settings)
+epidermis_layer = epidermis.geometrical_volume
+
+print("Plotting...")
+
+fig = plt.figure(figsize=(7, 7))
+ax = fig.add_subplot(111, projection='3d')
+ax.voxels(vessel_tree, shade=True, facecolors="red", alpha=0.55)
+ax.voxels(epidermis_layer, shade=True, facecolors="brown", alpha=0.55)
+ax.set_aspect('auto')
+ax.set_zlim(50, 0)
+ax.set_zlabel("Depth [mm]")
+ax.set_xlabel("x width [mm]")
+ax.set_ylabel("y width [mm]")
+ax.view_init(elev=10., azim=-45)
+plt.savefig("vessel_tree.png", dpi=300)
+plt.show()
